887e1f829016b439c8a6adb7e2772ceb31298be2
Tzikas <niko.tzikas@gmail.com>
2019-03-20 14:10:41 -0400

    adsf

---
+++ client/build/precache-manifest.018476e4a225b083d7f96a04a05420d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "626ec1363fcd3d8e7376",
+    "url": "/static/css/main.28cd23cd.chunk.css"
+  },
+  {
+    "revision": "626ec1363fcd3d8e7376",
+    "url": "/static/js/main.339f6d2a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c4fc9df80b46e035dbad",
+    "url": "/static/js/2.f6e59ad8.chunk.js"
+  },
+  {
+    "revision": "9d56970a932fdbe8aac71067063dac14",
+    "url": "/static/media/logo.9d56970a.svg"
+  },
+  {
+    "revision": "01942cc52925fbf116453fcd6c8a3349",
+    "url": "/index.html"
+  }
+];
